6b6bc9c792b3f1b183f31ebb5a26ffe2df0293ed
Aaron B <abriscoe2008@gmail.com>
2019-06-12 00:19:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,5 +172,22 @@ def player_stats(sought_player_name)
   new_hash
 end
 
+def big_shoe_rebounds
+  biggest_shoe = 0
+  num_rebounds = 0
+
+  game_hash.each do |place, team|
+    team[:players].each do |player|
+      if player[:shoe] > biggest_shoe
+        biggest_shoe = player[:shoe]
+        num_rebounds = player[:rebounds]
+      end
+    end
+  end
+  num_rebounds
+end
+
+
+
 
 
